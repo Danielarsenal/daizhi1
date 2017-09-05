a49b9d66dfe4d5023c323c306097071eaa20e3fe
danielnasri <danielnasri@sina.cn>
2017-09-05 11:04:40 +0800

    update

--- js/index.js
+++ js/index.js
@@ -21,34 +21,84 @@ $(function () {
                     /*当前自定义属性中 存储的图片路径*/
                     var imgSrc = item.data("largeImage");
                     /*添加非移动端的子元素*/
-                    item.html($('<a href="javascript:;" class="pcImg"></a>').css("backgroundImage", "url('" + imgSrc + "')"));
+                    item.html($('<div class="pcImg"></div>').css("backgroundImage", "url('" + imgSrc + "')"));
                 });
             }
             else {
                 $(items).each(function (index, value) {
                     var item = $(this);
                     var imgSrc = item.data("smallImage");
-                    item.html('<a href="javascript:;" class="mobileImg"><img src="' + imgSrc + '" alt="..."></a>');
+                    item.html('<div class="mobileImg"><img src="' + imgSrc + '" alt="..."></div>');
                 });
             }
         }).trigger("resize");
     }
-     //左侧导航栏
+    var width = $(window).width();
+    //主容器的位置
+
+
+
+
+
+    //ipad左侧导航栏
     $('.nav-left').click(function () {
         if ($(this).is('.nav-active')) {
-            $(this).animate({ left: 20 }, "fast");
+            $(this).animate({ left: 0, top: 8 });
+            $('#logo').animate({ left: 38 });
+            $('.ipad_nav_content').animate({ left: -240 });
+            $('.content-wrapper').css('position', 'static').css('marginLeft', 0);
+            $('.content-wrapper').animate({ left: 0 });
             $(this).removeClass('nav-active');
-            $('#logo').animate({ left: 120 }, "fast");
-            $('.nav-content').animate({ left: -240 },'slow');
-            $('#content').animate({ left: 0 });
-            
+            $('footer').css('display', 'block');
         } else {
-            $(this).animate({ left: 240 }, "fast");
+            $('footer').css('display', 'none');
+            $(this).animate({ left: 240, top: 16 });
+            $('#logo').animate({ left: 278 });
+            $('.ipad_nav_content').animate({ left: 0 });
+            if (width <= 1024) {/*说明非移动端*/
+                $('.content-wrapper').css('position', 'absolute');
+                var contentWidth = $('.content-wrapper').width();
+                console.log(contentWidth);
+                contentWidth /= -2;
+                console.log(contentWidth);
+                $('.content-wrapper').css('left', '50%').css('marginLeft', contentWidth + 'px');
+            }
+            $('.content-wrapper').animate({ marginLeft: contentWidth + 240 });
             $(this).addClass('nav-active');
-            $('#logo').animate({ left: 120 }, "fast");
-            $('.nav-content').animate({ left: 0 },'slow');
-            $('#content').animate({ left: 240 });
-            
+        }
+    });
+
+    //iphone左侧导航栏
+    $('.iphone_nav_left').click(function () {
+        if ($(this).is('.nav-active')) {
+            $(this).animate({ left: 15, top: 2 });
+            $('.language i').animate({ right: 15, top: 5 });
+            $('#iphone_logo').animate({ left: 15 });
+            $('.ipad_nav_content').animate({ left: -240 });
+            // $('.content-wrapper').css('width', width);
+            $('.content-wrapper').css('position', 'static').css('marginLeft', 0);
+            $('.content-wrapper').animate({ left: 0 });
+            $(this).removeClass('nav-active');
+            $('footer').css('display', 'block');
+
+        } else {
+            $('footer').css('display', 'none');
+            $(this).animate({ left: 255, top: 10 });
+            $('.language i').animate({ right: -255, top: 5 });
+            $('#iphone_logo').animate({ left: 255 });
+            $('.ipad_nav_content').animate({ left: 0 });
+            if (width <= 1024) {/*说明非移动端*/
+                $('.content-wrapper').css('position', 'absolute');
+                var contentWidth = $('.content-wrapper').width();
+                var width = contentWidth;
+                console.log(contentWidth);
+                contentWidth /= -2;
+                console.log(contentWidth);
+                $('.content-wrapper').css('left', '50%').css('marginLeft', contentWidth + 'px');
+            }
+            // $('.content-wrapper').css('width', width);
+            $('.content-wrapper').animate({ marginLeft: 240 });
+            $(this).addClass('nav-active');
         }
     })
 
